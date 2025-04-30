6c2c00066e97ded6a0f3806e51d3e04c89243eb5
Pedro Oliveira <pedricky63@gmail.com>
2025-04-30 01:11:12 +0000

    xadrez1.2

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
- 
-void moverTorre(int casas) {
+
+void moverTorre(int casas) { //recursividade Torre.
 
     if (casas > 0)
     {
@@ -10,17 +10,17 @@ void moverTorre(int casas) {
     
 }
 
-void moverRainha(int casas) {
+void moverRainha(int casas) {//recursividade Rainha.
 
     if (casas > 0)
     {
         printf("Direita\n");
-        moverTorre(casas - 1);
+        moverRainha(casas - 1);
     }   
     
 }
 
-void moverBispo(int casas) {
+void moverBispo(int casas) { //recursividade Bispo.
 
     if (casas > 0)
     {
@@ -32,33 +32,32 @@ void moverBispo(int casas) {
 
 int main (){
 
-    int linha = 1;
+    int linha = 1; //Incrementos para as informações do loop em do do while ("Cavalo linas 52 à 63").
     int cavalo = 1;
 
-    printf("Movimentos da Torre: \n");
+    printf("Movimentos da Torre: \n"); //Comando para loop da recursividade Torre.
     moverTorre(5);
     printf("\n");    
 
-    printf("Movimentos do Bispo: \n");
+    printf("Movimentos do Bispo: \n"); //Comando para loop da recursividade Bispo.
     moverBispo(5);
     printf("\n");
 
-    printf("Movimentos da Rainha: \n");
+    printf("Movimentos da Rainha: \n"); //Comando para loop da recursividade Rainha.
     moverRainha(8);
     printf("\n");
 
     printf("\n");
-    printf("Jogada do Cavalo!\n");
-    printf("\n");
+    printf("Jogada do Cavalo!\n"); //Inf da peça que fará o movimento.
     do
     {
         int cavalo = 1;
         do
         {
-            printf("%d Baixo!\n", linha + cavalo);
+            printf("%d Cima!\n", linha + cavalo); //print para o loop do Cavalo "para cima".
             cavalo++;
         } while (cavalo <= linha);
-        printf("%d Esquerda!\n", linha);
+        printf("%d Direita!\n", linha); //print para o loop do Cavalo "para Direita".
         linha++;
         
     } while (linha <= cavalo);
